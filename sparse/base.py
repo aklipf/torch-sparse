a6d638e62c2e977a9470b635b27ae30014504ced
Astrid Klipfel <astridklipfel@gmail.com>
2024-04-17 17:34:01 +0200

    unittest base

--- sparse/base.py
+++ sparse/base.py
@@ -1,8 +1,6 @@
-from __future__ import annotations
-from typing import Iterable, List, Tuple, Literal, Any
+from typing import Iterable, List
 
 import torch
-import torch.nn.functional as F
 
 from .typing import Self
 
@@ -19,6 +17,8 @@ class BaseSparse(object):
         assert values is None or values.ndim in (1, 2)
         assert values is None or indices.shape[1] == values.shape[0]
         assert shape is None or indices.shape[0] == len(shape)
+        assert shape is not None or indices.shape[1] != 0
+        assert values is None or indices.device == values.device
 
         if shape is None:
             shape = tuple((indices.amax(dim=1) + 1).tolist())
@@ -26,61 +26,20 @@ class BaseSparse(object):
         if values is not None and values.ndim == 1:
             values.unsqueeze_(1)
 
-        self.shape = tuple(shape)
+        self.__shape = tuple(shape)
         self.indices = indices
         self.values = values
 
-        if sort and (not self.is_sorted()):
-            self.sort_indices_()
+        if sort and (not self._is_sorted()):
+            self._sort_indices_()
 
-    def __included_dims(self, except_dim: int | Iterable = None) -> List[int]:
-        if isinstance(except_dim, int):
-            except_dim = {except_dim}
-        elif except_dim is None:
-            except_dim = set()
-        else:
-            except_dim = set(except_dim)
-
-        return sorted(list(set(self.dims) - except_dim))
-
-    def argsort_indices(self, except_dim: int | Iterable = None) -> torch.LongTensor:
-        dims = self.__included_dims(except_dim)
-
-        perm = None
-
-        for i in reversed(dims):
-
-            if perm is None:
-                current_indices = self.indices[i]
-            else:
-                current_indices = self.indices[i, perm]
-
-            current_perm = torch.argsort(current_indices, stable=True)
-
-            if perm is None:
-                perm = current_perm
-            else:
-                perm = perm[current_perm]
-
-        return perm
-
-    def sort_indices_(self):
-        """Sort indices and values"""
-
-        perm = self.argsort_indices()
-
-        # apply reindexing
-        self.indices = self.indices[:, perm]
-
-        if self.values is not None:
-            self.values = self.values[:, perm]
-
-    def is_sorted(self) -> bool:
-        return (self.indices[:, 1:] <= self.indices[:, :-1]).all()
+    @property
+    def shape(self) -> tuple:
+        return self.__shape
 
     @property
     def dims(self) -> tuple:
-        return tuple(range(len(self.shape)))
+        return tuple(range(len(self.__shape)))
 
     @property
     def dtype(self) -> type:
@@ -94,78 +53,46 @@ class BaseSparse(object):
         return self.indices.device
 
     def to(self, device: torch.device) -> Self:
-        if self.values is None:
-            return self.__class__(
-                indices=self.indices.to(device),
-                values=None,
-                shape=self.shape,
-            )
-
         return self.__class__(
             indices=self.indices.to(device),
-            values=self.values.to(device),
-            shape=self.shape,
+            values=None if self.values is None else self.values.to(device),
+            shape=self.__shape,
         )
 
     def clone(self) -> Self:
-        if self.values is None:
-            return self.__class__(
-                indices=self.indices.clone(), values=None, shape=self.shape
-            )
-
         return self.__class__(
-            indices=self.indices.clone(), values=self.values.clone(), shape=self.shape
+            indices=self.indices.clone(),
+            values=None if self.values is None else self.values.clone(),
+            shape=self.__shape,
         )
 
     def detach(self) -> Self:
-        if self.values is None:
-            return self.__class__(
-                indices=self.indices.detach(), values=None, shape=self.shape
-            )
-
         return self.__class__(
-            indices=self.indices.detach(), values=self.values.detach(), shape=self.shape
+            indices=self.indices.detach(),
+            values=None if self.values is None else self.values.detach(),
+            shape=self.__shape,
         )
 
-    def mask_(self, mask: torch.BoolTensor) -> Self:
-        assert mask.ndim == 1 and mask.shape[0] == self.indices.shape[1]
-
-        self.indices = self.indices[:, mask]
-
-        if self.values is not None:
-            self.values = self.values[mask]
-
-        return self
-
-    def mask(self, mask: torch.BoolTensor) -> Self:
-        sparse = self.clone()
-        sparse.mask_(mask)
-
-        return sparse
-
     def __repr__(self) -> str:
-        if self.values is None:
-            return f"""{self.__class__.__name__}(shape={self.shape},
-  indices={self.indices},
-  device=\"{self.device}\")"""
-
-        return f"""{self.__class__.__name__}(shape={self.shape},
+        return f"""{self.__class__.__name__}(shape={self.__shape},
   indices={self.indices},
   values={self.values},
   device=\"{self.device}\")"""
 
     @property
     def dense(self) -> torch.Tensor:
-        if self.values is None:
-            shape = self.shape
+        if self.values is None or self.values.shape[1] == 1:
+            shape = self.__shape
         else:
-            shape = self.shape + self.values.shape[1:]
+            shape = self.__shape + self.values.shape[1:]
 
         x = torch.zeros(shape, dtype=self.dtype, device=self.device)
         indices = [self.indices[i] for i in range(self.indices.shape[0])]
 
         if self.values is None:
             x[indices] = 1
+        elif self.values.shape[1] == 1:
+            x[indices] = self.values.flatten()
         else:
             x[indices] = self.values
 
@@ -185,18 +112,64 @@ class BaseSparse(object):
 
         return lst_dim
 
-    def _global_index(self, without_dim: List[int] = None) -> torch.LongTensor:
-        shape = torch.tensor(self.shape, dtype=torch.long)
+    def _included_dims(self, except_dim: int | Iterable = None) -> List[int]:
+        if isinstance(except_dim, int):
+            except_dim = {except_dim}
+        elif except_dim is None:
+            except_dim = set()
+        else:
+            except_dim = set(except_dim)
 
-        if without_dim is not None:
-            shape[without_dim] = 1
+        return sorted(list(set(self.dims) - except_dim))
 
-        prod_tensor = F.pad(shape, (1, 0), value=1).cumprod(0)
+    def _argsort_indices(self, except_dim: int | Iterable = None) -> torch.LongTensor:
+        dims = self._included_dims(except_dim)
 
-        if without_dim is not None:
-            prod_tensor[without_dim] = 0
+        perm = None
 
-        return (self.indices * prod_tensor.to(self.device)[:-1, None]).sum(dim=0)
+        for i in reversed(dims):
+
+            if perm is None:
+                current_indices = self.indices[i]
+            else:
+                current_indices = self.indices[i, perm]
+
+            current_perm = torch.argsort(current_indices, stable=True)
+
+            if perm is None:
+                perm = current_perm
+            else:
+                perm = perm[current_perm]
+
+        return perm
+
+    def _sort_indices_(self):
+        """Sort indices and values"""
+
+        perm = self._argsort_indices()
+
+        # apply reindexing
+        self.indices = self.indices[:, perm]
+
+        if self.values is not None:
+            self.values = self.values[perm]
+
+    def _is_sorted(self) -> bool:
+        sorted_mask = self.indices.diff(dim=1) <= 0
+        unsorted_mask = self.indices.diff(dim=1) >= 0
+
+        dims = torch.tensor([self.dims], device=self.device).t()
+        dims = dims.repeat(1, sorted_mask.shape[1])
+
+        min_sorted = dims.clone()
+        min_sorted[sorted_mask] = len(self.shape)
+        min_sorted = min_sorted.amin(dim=0)
+
+        min_unsorted = dims.clone()
+        min_unsorted[unsorted_mask] = len(self.shape)
+        min_unsorted = min_unsorted.amin(dim=0)
+
+        return (min_sorted < min_unsorted).all()
 
     @classmethod
     def _prod(cls, values: Iterable[int]) -> int:
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,411 @@
 import pytest
+import torch
+import unittest.mock as mock
+
+from sparse.typing import Self
+from sparse.base import BaseSparse
 
 
-def test_base():
-    assert True
+class MockTensor:
+    def __init__(
+        self,
+        shape: tuple = None,
+        dtype: type = torch.float32,
+        device: torch.device = torch.device("cpu"),
+    ):
+        self.shape = shape
+        self.dtype = dtype
+        self.device = device
+
+    @property
+    def ndim(self):
+        return len(self.shape)
+
+    def amax(self, **kwargs) -> torch.LongTensor:
+        return torch.ones(self.shape[0], dtype=torch.long)
+
+    def diff(self, **kwargs) -> torch.LongTensor:
+        return torch.tensor([[1]]).repeat(self.shape[0], 1)
+
+    def __getitem__(self, *args):
+        return torch.tensor(True)
+
+    def __repr__(self) -> str:
+        return (
+            f"MockTensor(shape={self.shape}, dtype={self.dtype}, device={self.device})"
+        )
+
+
+def test_base_sort():
+    indices = torch.tensor(
+        [
+            [0, 0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4, 5, 5, 6],
+        ]
+    )
+    perm = torch.randperm(indices.shape[1])
+
+    sparse = BaseSparse(indices=indices[:, perm])
+
+    assert (sparse.indices == indices).all().item()
+
+    indices = torch.tensor(
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+            [0, 0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4, 5, 5, 6],
+            [0, 2, 1, 3, 3, 2, 2, 2, 4, 3, 5, 4, 5, 5, 6],
+        ]
+    )
+    perm = torch.randperm(indices.shape[1])
+
+    sparse = BaseSparse(indices=indices[:, perm])
+
+    assert (sparse.indices == indices).all().item()
+
+
+def test_base_no_sort():
+    indices = torch.tensor(
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+            [0, 0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4, 5, 5, 6],
+            [0, 2, 1, 3, 3, 2, 2, 2, 4, 3, 5, 4, 5, 5, 6],
+        ]
+    )
+    perm = torch.randperm(indices.shape[1])
+
+    sparse = BaseSparse(indices=indices[:, perm], sort=False)
+
+    assert (sparse.indices == indices[:, perm]).all().item()
+
+
+def test_base_sort_with_values():
+    indices = torch.randperm(128)[:12].sort().values.unsqueeze(0)
+    values = torch.arange(indices.shape[1], dtype=torch.long)
+    perm = torch.randperm(indices.shape[1])
+
+    sparse = BaseSparse(indices=indices[:, perm], values=values[perm])
+
+    assert (sparse.indices == indices).all().item()
+    assert (sparse.values.flatten() == values).all().item()
+    assert sparse.values.shape == (12, 1)
+
+    sparse = BaseSparse(indices=indices[:, perm], values=values[perm].unsqueeze(1))
+
+    assert (sparse.indices == indices).all().item()
+    assert (sparse.values.flatten() == values).all().item()
+    assert sparse.values.shape == (12, 1)
+
+
+def test_base_assert():
+    # assert indices ndim and dtype
+    with pytest.raises(AssertionError):
+        BaseSparse(torch.tensor([]))
+
+    BaseSparse(torch.tensor([[0]], dtype=torch.long))
+
+    with pytest.raises(AssertionError):
+        BaseSparse(torch.tensor([[0]], dtype=torch.float32))
+
+    # assert values ndim
+    BaseSparse(torch.tensor([[0]], dtype=torch.long), values=torch.tensor([0]))
+    BaseSparse(torch.tensor([[0]], dtype=torch.long), values=torch.tensor([[0]]))
+
+    with pytest.raises(AssertionError):
+        BaseSparse(torch.tensor([[0]], dtype=torch.long), values=torch.tensor(0))
+
+    with pytest.raises(AssertionError):
+        BaseSparse(torch.tensor([[0]], dtype=torch.long), values=torch.tensor([[[0]]]))
+
+    # assert compare indices shape and values shape
+    with pytest.raises(AssertionError):
+        BaseSparse(
+            torch.tensor([[0]], dtype=torch.long), values=torch.tensor([[[0, 0]]])
+        )
+
+    # assert compare indices shape and shape parameter
+    BaseSparse(torch.tensor([[0]], dtype=torch.long), shape=(2,))
+    BaseSparse(torch.tensor([[0], [0]], dtype=torch.long), shape=(2, 3))
+
+    with pytest.raises(AssertionError):
+        BaseSparse(torch.tensor([[0]], dtype=torch.long), shape=(2, 3))
+
+    with pytest.raises(AssertionError):
+        BaseSparse(torch.tensor([[0], [0]], dtype=torch.long), shape=(2,))
+
+    # assert not empty when auto detect shape
+    with pytest.raises(AssertionError):
+        BaseSparse(torch.tensor([[]], dtype=torch.long))
+    with pytest.raises(AssertionError):
+        BaseSparse(torch.tensor([[], [], []], dtype=torch.long))
+
+
+def test_base_shape():
+    sparse = BaseSparse(MockTensor(shape=(1, 3), dtype=torch.long), shape=(5,))
+    assert sparse.shape == (5,)
+
+    sparse = BaseSparse(MockTensor(shape=(2, 3), dtype=torch.long), shape=(12, 4))
+    assert sparse.shape == (12, 4)
+
+    indices = MockTensor(shape=(1, 3), dtype=torch.long)
+    indices.amax = mock.Mock("amax", return_value=torch.tensor([3], dtype=torch.long))
+
+    sparse = BaseSparse(indices)
+    assert sparse.shape == (4,)
+
+    indices.amax.assert_called_once_with(dim=1)
+
+    indices = MockTensor(shape=(2, 3), dtype=torch.long)
+    indices.amax = mock.Mock(
+        "amax", return_value=torch.tensor([3, 5], dtype=torch.long)
+    )
+
+    sparse = BaseSparse(indices)
+    assert sparse.shape == (4, 6)
+
+    indices.amax.assert_called_once_with(dim=1)
+
+
+def test_base_dims():
+    assert BaseSparse(MockTensor(shape=(1, 3), dtype=torch.long)).dims == (0,)
+    assert BaseSparse(MockTensor(shape=(2, 3), dtype=torch.long)).dims == (0, 1)
+    assert BaseSparse(MockTensor(shape=(3, 3), dtype=torch.long)).dims == (0, 1, 2)
+
+
+def test_base_dtype():
+    assert BaseSparse(MockTensor(shape=(2, 3), dtype=torch.long)).dtype == torch.float32
+    assert (
+        BaseSparse(
+            MockTensor(shape=(2, 3), dtype=torch.long),
+            values=MockTensor(shape=(3, 1), dtype=torch.long),
+        ).dtype
+        == torch.long
+    )
+    assert (
+        BaseSparse(
+            MockTensor(shape=(2, 3), dtype=torch.long),
+            values=MockTensor(shape=(3, 1), dtype=torch.float32),
+        ).dtype
+        == torch.float32
+    )
+
+
+def test_base_device():
+    assert BaseSparse(
+        MockTensor(shape=(2, 3), dtype=torch.long, device=torch.device("cuda")),
+        sort=False,
+    ).device == torch.device("cuda")
+
+
+def test_base_to_without_values():
+    mocked_sparce = BaseSparse(indices=MockTensor(shape=(2, 3), dtype=torch.long))
+
+    mocked_sparce.indices.to = mock.MagicMock(
+        name="to", return_value=MockTensor(shape=(2, 3), dtype=torch.long)
+    )
+
+    mocked_sparce.to("cuda")
+
+    mocked_sparce.indices.to.assert_called_once_with("cuda")
+
+
+def test_base_to_with_values():
+    mocked_sparce = BaseSparse(
+        indices=MockTensor(shape=(2, 3), dtype=torch.long),
+        values=MockTensor(shape=(3, 1)),
+    )
+
+    mocked_sparce.indices.to = mock.MagicMock(
+        name="to", return_value=MockTensor(shape=(2, 3), dtype=torch.long)
+    )
+    mocked_sparce.values.to = mock.MagicMock(
+        name="to", return_value=MockTensor(shape=(3, 1))
+    )
+
+    mocked_sparce.to("cuda")
+
+    mocked_sparce.indices.to.assert_called_once_with("cuda")
+    mocked_sparce.values.to.assert_called_once_with("cuda")
+
+
+def test_base_clone_without_values():
+    mocked_sparce = BaseSparse(indices=MockTensor(shape=(2, 3), dtype=torch.long))
+
+    mocked_sparce.indices.clone = mock.MagicMock(
+        name="clone", return_value=MockTensor(shape=(2, 3), dtype=torch.long)
+    )
+
+    mocked_sparce.clone()
+    mocked_sparce.indices.clone.assert_called_once_with()
+
+
+def test_base_clone_with_values():
+    mocked_sparce = BaseSparse(
+        indices=MockTensor(shape=(2, 3), dtype=torch.long),
+        values=MockTensor(shape=(3, 1)),
+    )
+
+    mocked_sparce.indices.clone = mock.MagicMock(
+        name="clone", return_value=MockTensor(shape=(2, 3), dtype=torch.long)
+    )
+    mocked_sparce.values.clone = mock.MagicMock(
+        name="clone", return_value=MockTensor(shape=(3, 1))
+    )
+
+    mocked_sparce.clone()
+
+    mocked_sparce.indices.clone.assert_called_once_with()
+    mocked_sparce.values.clone.assert_called_once_with()
+
+
+def test_base_detach_without_values():
+    mocked_sparce = BaseSparse(indices=MockTensor(shape=(2, 3), dtype=torch.long))
+
+    mocked_sparce.indices.detach = mock.MagicMock(
+        name="detach", return_value=MockTensor(shape=(2, 3), dtype=torch.long)
+    )
+
+    mocked_sparce.detach()
+    mocked_sparce.indices.detach.assert_called_once_with()
+
+
+def test_base_detach_with_values():
+    mocked_sparce = BaseSparse(
+        indices=MockTensor(shape=(2, 3), dtype=torch.long),
+        values=MockTensor(shape=(3, 1)),
+    )
+
+    mocked_sparce.indices.detach = mock.MagicMock(
+        name="detach", return_value=MockTensor(shape=(2, 3), dtype=torch.long)
+    )
+    mocked_sparce.values.detach = mock.MagicMock(
+        name="detach", return_value=MockTensor(shape=(3, 1))
+    )
+
+    mocked_sparce.detach()
+
+    mocked_sparce.indices.detach.assert_called_once_with()
+    mocked_sparce.values.detach.assert_called_once_with()
+
+
+def test_base_repr():
+    mocked_sparce = BaseSparse(
+        indices=MockTensor(shape=(2, 3), dtype=torch.long),
+        values=MockTensor(shape=(3, 1)),
+    )
+
+    assert (
+        repr(mocked_sparce)
+        == """BaseSparse(shape=(2, 2),
+  indices=MockTensor(shape=(2, 3), dtype=torch.int64, device=cpu),
+  values=MockTensor(shape=(3, 1), dtype=torch.float32, device=cpu),
+  device="cpu")"""
+    )
+
+
+def test_base_dense():
+    indices = torch.tensor([[0, 1, 2, 2], [0, 1, 0, 2]])
+
+    assert (
+        BaseSparse(indices).dense == torch.tensor([[1, 0, 0], [0, 1, 0], [1, 0, 1]])
+    ).all()
+
+    assert (
+        BaseSparse(indices, torch.tensor([1, 2, 3, 4])).dense
+        == torch.tensor([[1, 0, 0], [0, 2, 0], [3, 0, 4]])
+    ).all()
+
+    assert (
+        BaseSparse(indices, torch.tensor([[1, 5], [2, 6], [3, 7], [4, 8]])).dense
+        == torch.tensor(
+            [
+                [[1, 5], [0, 0], [0, 0]],
+                [[0, 0], [2, 6], [0, 0]],
+                [[3, 7], [0, 0], [4, 8]],
+            ]
+        )
+    ).all()
+
+
+def test_base_dim_to_list():
+    assert BaseSparse._dim_to_list() == []
+    assert BaseSparse._dim_to_list(dim=None) == []
+    assert BaseSparse._dim_to_list(dim=3) == [3]
+
+    assert BaseSparse._dim_to_list(dim=(1, 2)) == [1, 2]
+
+    with pytest.raises(AssertionError):
+        BaseSparse._dim_to_list(dim=(1, 2, 2))
+
+
+def test_base_included_dims():
+    mocked_sparce = BaseSparse(
+        indices=MockTensor(shape=(4, 3), dtype=torch.long),
+        values=MockTensor(shape=(3, 1)),
+    )
+
+    assert mocked_sparce._included_dims() == [0, 1, 2, 3]
+    assert mocked_sparce._included_dims(except_dim=1) == [0, 2, 3]
+    assert mocked_sparce._included_dims(except_dim=(1, 2)) == [0, 3]
+    assert mocked_sparce._included_dims(except_dim=(1, 2, 1, 2)) == [0, 3]
+    assert mocked_sparce._included_dims(except_dim=(2, 3, 1)) == [0]
+
+
+def test_base_argsort_indices():
+    indices = torch.randint(0, 1024, (3, 1024))
+    sparse = BaseSparse(indices, sort=False)
+
+    perm = sparse._argsort_indices()
+    sorted = (
+        (sparse.indices[0, perm] << 20)
+        + (sparse.indices[1, perm] << 10)
+        + sparse.indices[2, perm]
+    )
+    assert (sorted.diff() >= 0).all()
+
+    perm = sparse._argsort_indices(1)
+    sorted = (sparse.indices[0, perm] << 10) + sparse.indices[2, perm]
+    assert (sorted.diff() >= 0).all()
+
+    perm = sparse._argsort_indices((0, 1))
+    sorted = sparse.indices[2, perm]
+    assert (sorted.diff() >= 0).all()
+
+    perm = sparse._argsort_indices((1, 2))
+    sorted = sparse.indices[0, perm]
+    assert (sorted.diff() >= 0).all()
+
+
+def test_base_sort_indices():
+    indices = torch.randint(0, 1024, (3, 1024))
+    sparse = BaseSparse(indices.clone(), sort=False)
+
+    sparse._sort_indices_()
+    sorted = (sparse.indices[0] << 20) + (sparse.indices[1] << 10) + sparse.indices[2]
+    assert (sorted.diff() >= 0).all()
+
+    indices = torch.randperm(1024).unsqueeze(0)
+    values = torch.randn(1024)
+    sparse = BaseSparse(indices.clone(), values.clone(), sort=False)
+
+    sparse._sort_indices_()
+    assert (values == sparse.values[indices[0]].flatten()).all()
+
+
+def test_base_is_sorted():
+    sorted = torch.randint(0, 1 << 30, (64,)).sort().values
+    indices = torch.stack((sorted >> 20, (sorted >> 10) & 0x3FF, sorted & 0x3FF))
+
+    sparse = BaseSparse(indices.clone(), sort=False)
+    assert sparse._is_sorted()
+
+    indices = torch.randint(0, 1024, (3, 1024))
+
+    sparse = BaseSparse(indices.clone(), sort=False)
+    assert not sparse._is_sorted()
+
+
+def test_base_prod():
+    assert BaseSparse._prod([]) == 1
+    assert BaseSparse._prod([1]) == 1
+    assert BaseSparse._prod([2]) == 2
+    assert BaseSparse._prod([2, 3]) == 6
+    assert BaseSparse._prod([2, 3, 4]) == 24
