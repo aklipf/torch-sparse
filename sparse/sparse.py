f35beebb96fb7254f3f47cc718e239a9acf1ab52
Astrid Klipfel <astridklipfel@gmail.com>
2024-04-23 17:11:07 +0200

    remove prod

--- sparse/sparse.py
+++ sparse/sparse.py
@@ -3,12 +3,10 @@ from .shape import SparseShapeMixin
 from .type import SparseTypeMixin
 from .scatter import SparseScatterMixin
 from .cat import SparseCatMixin
-from .prod import SparseProdMixin
 from .mask import SparseMaskMixin
 
 
 class SparseTensor(
-    SparseProdMixin,
     SparseCatMixin,
     SparseScatterMixin,
     SparseShapeMixin,
