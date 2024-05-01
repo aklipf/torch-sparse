44a2db632b7d4618d2760a25a058261b1aebcd29
Astrid Klipfel <astridklipfel@gmail.com>
2024-05-01 22:56:17 +0200

    add repeat_last_dims (wip)

--- sparse/__init__.py
+++ sparse/__init__.py
@@ -1,4 +1,5 @@
 from .sparse import SparseTensor
+from .mapping import Mapping
 
 __version__ = "0.1.1"
-__all__ = ["SparseTensor"]
+__all__ = ["SparseTensor", "Mapping"]
--- sparse/mapping.py
+++ sparse/mapping.py
@@ -1,21 +1,82 @@
 from __future__ import annotations
+from typing import List, Tuple
 
 import torch
+import torch.nn.functional as F
 
+from .typing import Self
 from .sparse import SparseTensor
 
 
 class Mapping:
     def __init__(
-        self, source: SparseTensor, target: SparseTensor, indices: torch.LongTensor
+        self, source: SparseTensor, target: SparseTensor, mapping: torch.LongTensor
     ):
-        self._mapping = indices
+        self._mapping = mapping
         self._source = source
         self._target = target
 
+    @staticmethod
+    def _count_repeating_indices(
+        indices: torch.LongTensor,
+    ) -> Tuple[torch.LongTensor, torch.LongTensor]:
+        change = F.pad(
+            (indices[:, :-1] != indices[:, 1:]).any(dim=0), (1, 0), value=True
+        )
+        _, count = torch.unique_consecutive(change.cumsum(0), return_counts=True)
+        unique_indices = indices[:, change]
+
+        return unique_indices, count
+
     @classmethod
-    def broadcast(cls, source: SparseTensor, target_dims: tuple) -> Mapping:
-        pass
+    def repeat_last_dims(
+        cls, source: SparseTensor, ndim: int, repeat: int = 2
+    ) -> Mapping:
+        boadcast_indices, mapping = cls._repeat_last_dims(source.indices, ndim, repeat)
+
+        target = SparseTensor()  # TODO
+        return cls(source=source, target=target, mapping=mapping)
+
+    @classmethod
+    def _repeat_last_dims(
+        cls, indices: torch.LongTensor, ndim: int, repeat: int
+    ) -> Tuple[torch.LongTensor, torch.LongTensor]:
+        assert ndim > 0 and repeat > 1
+        assert ndim <= indices.shape[0]
+
+        # calculate base and reprtition
+        unique_indices, count = Mapping._count_repeating_indices(indices[:-ndim])
+        count_repeat = count.pow(repeat)
+
+        repeated_base = unique_indices.repeat_interleave(count_repeat, dim=1)
+
+        # calculating indexing of the repeated dims
+        ptr_base = F.pad(count.cumsum(0), (1, 0))
+        ptr_top = F.pad(count_repeat.cumsum(0), (1, 0))
+        batch_top = torch.arange(
+            count_repeat.shape[0], dtype=torch.long, device=indices.device
+        ).repeat_interleave(count_repeat)
+        idx_top = (
+            torch.arange(batch_top.shape[0], dtype=torch.long, device=indices.device)
+            - ptr_top[batch_top]
+        )
+
+        exp_top = F.pad(
+            count[None, :].repeat(repeat - 1, 1).cumprod(0), (0, 0, 1, 0), value=1
+        ).flip(0)
+
+        idx_top = (
+            idx_top[None, :] // exp_top[:, batch_top] % count[None, batch_top]
+            + ptr_base[None, batch_top]
+        )
+
+        # repeat top dims and concatenate with base
+        repeated_top = (
+            indices[-ndim:, idx_top].swapdims(0, 1).reshape(ndim * repeat, -1)
+        )
+        result_indices = torch.cat((repeated_base, repeated_top), dim=0)
+
+        return result_indices, idx_top[0]
 
     def is_source(self, tensor: SparseTensor) -> bool:
         return id(self._source.indices) == id(tensor.indices)
@@ -42,3 +103,13 @@ class Mapping:
     @property
     def mapping(self) -> torch.LongTensor:
         return self._mapping
+
+
+if __name__ == "__main__":
+    tensor = SparseTensor(torch.randint(0, 4, (3, 16)))
+    result, batch = Mapping.repeat_last_dims(tensor.indices, 2, 2)
+    print(tensor.indices)
+    sorted_tensor = SparseTensor(result)
+    print(result)
+    print(batch)
+    assert (result == sorted_tensor.indices).all().item()
